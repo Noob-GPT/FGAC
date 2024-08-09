3f37ed81bf652f10a10d76cdfc9ed6d332aef52a
sibjagun <sibjagun99@kyungmin.ac.kr>
2024-08-09 15:36:38 +0900

    Add: "BackEnd 추가"

---
+++ backend/src/main/java/com/npt/fgac/FgacApplication.java
@@ -0,0 +1,13 @@
+package com.npt.fgac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FgacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FgacApplication.class, args);
+	}
+
+}
